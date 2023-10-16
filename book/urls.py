c9ad8e76b7aec52111757b9ff08e46244f94cfe1
hiroki3905 <96215671+hiroki3905@users.noreply.github.com>
2023-10-16 18:32:33 +0900

    更新機能追加

--- book/urls.py
+++ book/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='book-detail'),
     path('book/create/', views.CreateBookView.as_view(),name='list-book'),
     path('book/<int:pk>/delete/', views.DeleteBookView.as_view(), name='book-delete'),
+    path('book/<int:pk>/update/', views.DeleteBookView.as_view(), name='book-update'),
 ]
